1b2a9faa2fc69ee017090b39030ffde53a24743f
Robert Biddle <Robert.D.Biddle@gmail.com>
2016-08-29 14:03:48 -0400

    initial commit

---
+++ ActiveDirectoryForest.psm1
@@ -0,0 +1,36 @@
+﻿function Get-ForestComputers
+{
+    (Get-ADForest).Domains | ForEach-Object {
+        ((Get-ADDomainController -Discover -DomainName $_ -ForceDiscover).HostName)[0] | ForEach-Object {
+            Get-ADComputer -Filter * -Server $_
+        }       
+    }
+}
+
+function Get-ForestUsers
+{
+    (Get-ADForest).Domains | ForEach-Object {
+        ((Get-ADDomainController -Discover -DomainName $_ -ForceDiscover).HostName)[0]  | ForEach-Object {
+            Get-ADUser -Filter * -Server $_
+        }
+    }    
+}
+
+function Get-ForestComputerObjects
+{
+    Get-ADObject -Filter 'ObjectClass -eq "computer"' -SearchBase "$((Get-ADDomain (Get-ADForest).Name).DistinguishedName)" -server "$(((Get-ADForest).GlobalCatalogs)[0]):3268"
+}
+
+function Get-ForestUserObjects
+{
+    Get-ADObject -Filter 'ObjectClass -eq "user"' -SearchBase "$((Get-ADDomain (Get-ADForest).Name).DistinguishedName)" -server "$(((Get-ADForest).GlobalCatalogs)[0]):3268"
+}
+
+function Get-ForestObjects
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$filter
+    )
+    Get-ADObject $filter -SearchBase "$((Get-ADDomain (Get-ADForest).Name).DistinguishedName)" -server "$(((Get-ADForest).GlobalCatalogs)[0]):3268"
+}
