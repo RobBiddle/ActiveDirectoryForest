367f517b6a6ee31db64d169cd5458ba327dcfa68
Robert Biddle <Robert.D.Biddle@gmail.com>
2017-10-03 15:00:49 -0400

    Added Get-ForestDomainControllers function Added Properties Parameter to functions

--- ActiveDirectoryForest.psm1
+++ ActiveDirectoryForest.psm1
@@ -1,38 +1,46 @@
-﻿function Get-ForestComputers
+﻿function Get-ForestDomainControllers 
+{
+    (Get-ADForest).Domains | ForEach-Object {
+        Get-ADDomainController -Discover -DomainName $_ -ForceDiscover
+    }
+}
+
+function Get-ForestComputers ($Properties="Name")
 {
     (Get-ADForest).Domains | ForEach-Object {
         ((Get-ADDomainController -Discover -DomainName $_ -ForceDiscover).HostName)[0] | ForEach-Object {
-            Get-ADComputer -Filter * -Server $_ -Properties *
+            Get-ADComputer -Filter * -Server $_ -Properties $Properties
         }       
     }
 }
 
-function Get-ForestUsers
+function Get-ForestUsers ($Properties="Name")
 {
     (Get-ADForest).Domains | ForEach-Object {
         ((Get-ADDomainController -Discover -DomainName $_ -ForceDiscover).HostName)[0]  | ForEach-Object {
-            Get-ADUser -Filter * -Server $_ -Properties *
+            Get-ADUser -Filter * -Server $_ -Properties $Properties
         }
     }    
 }
 
-function Get-ForestComputerObjects
+function Get-ForestComputerObjects ($Properties="Name")
 {
-    Get-ADObject -Filter 'ObjectClass -eq "computer"' -SearchBase "$((Get-ADDomain (Get-ADForest).Name).DistinguishedName)" -server "$(((Get-ADForest).GlobalCatalogs)[0]):3268" -Properties *
+    Get-ADObject -Filter 'ObjectClass -eq "computer"' -SearchBase "$((Get-ADDomain (Get-ADForest).Name).DistinguishedName)" -server "$(((Get-ADForest).GlobalCatalogs)[0]):3268" -Properties $Properties
 }
 
-function Get-ForestUserObjects
+function Get-ForestUserObjects ($Properties="Name")
 {
-    Get-ADObject -Filter 'ObjectClass -eq "user"' -SearchBase "$((Get-ADDomain (Get-ADForest).Name).DistinguishedName)" -server "$(((Get-ADForest).GlobalCatalogs)[0]):3268" -Properties *
+    Get-ADObject -Filter 'ObjectClass -eq "user"' -SearchBase "$((Get-ADDomain (Get-ADForest).Name).DistinguishedName)" -server "$(((Get-ADForest).GlobalCatalogs)[0]):3268" -Properties $Properties
 }
 
 function Get-ForestObjects
 {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$filter
+        [string]$filter,
+        [string]$Properties="Name"
     )
-    Get-ADObject $filter -SearchBase "$((Get-ADDomain (Get-ADForest).Name).DistinguishedName)" -server "$(((Get-ADForest).GlobalCatalogs)[0]):3268" -Properties *
+    Get-ADObject $filter -SearchBase "$((Get-ADDomain (Get-ADForest).Name).DistinguishedName)" -server "$(((Get-ADForest).GlobalCatalogs)[0]):3268" -Properties $Properties
 }
 
 Export-ModuleMember -Function * -Variable * -Cmdlet *
